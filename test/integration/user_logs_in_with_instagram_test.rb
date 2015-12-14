a15affa5b0289becd18039c58631d4e6a6dc5f5b
acareaga <aaroncareaga@gmail.com>
2015-12-14 10:43:30 -0700

    Setup integration test for instagram login process

---
+++ test/integration/user_logs_in_with_instagram_test.rb
@@ -0,0 +1,39 @@
+require "test_helper"
+
+class UserLogsInWithTwitterTest < ActionDispatch::IntegrationTest
+  include Capybara::DSL
+
+  def setup
+    Capybara.app = AdventureJournal::Application
+    stub_omniauth
+  end
+
+  test "logging in" do
+    visit "/"
+    assert_equal 200, page.status_code
+    click_link "login"
+    assert_equal "/", current_path
+    assert page.has_content?("Aaron")
+    assert page.has_link?("logout")
+  end
+
+  def stub_omniauth
+    OmniAuth.config.test_mode = true
+    # then, provide a set of fake oauth data that
+    # omniauth will use when a user tries to authenticate:
+    OmniAuth.config.mock_auth[:instagram] = OmniAuth::AuthHash.new({
+      provider: 'instagram',
+      extra: {
+        raw_info: {
+          user_id: "1234",
+          name: "Aaron",
+          screen_name: "powskiing",
+        }
+      },
+      credentials: {
+        token: "pizza",
+        secret: "secretpizza"
+      }
+    })
+  end
+end
