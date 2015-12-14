871ff632a1cc3a5f3aa43f42eb32530d17b8b0c9
acareaga <aaroncareaga@gmail.com>
2015-12-14 11:09:42 -0700

    Added figaro and set up gitignore

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    reset_session
+    session.delete(:user_id)
     redirect_to root_path
   end
 end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :instagram, "2dd2c36619fc4924bf4d70b7df40091c", "17878ae5993b483095fdc84a54c367b7"
+  provider :instagram, ENV["insta_client_id"], ENV["insta_secret_key"]
 end
