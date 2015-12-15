690b2810ba84bb6e671c844766b52447d3901215
acareaga <aaroncareaga@gmail.com>
2015-12-15 13:23:46 -0700

    Added production keys

--- config/application.rb
+++ config/application.rb
@@ -8,10 +8,10 @@ Bundler.require(*Rails.groups)
 module AdventureJournal
   class Application < Rails::Application
 
-    Instagram.configure do |config|
-      config.client_id = ENV["insta_client_id"]
-      config.client_secret = ENV["insta_secret_key"]
-    end
+    # Instagram.configure do |config|
+    #   config.client_id = ENV["insta_client_id"]
+    #   config.client_secret = ENV["insta_secret_key"]
+    # end
 
     config.active_record.raise_in_transactional_callbacks = true
   end
