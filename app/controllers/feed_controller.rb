d63beccaf8b8aa243ae2d9ee3ba38b3b82bba90a
acareaga <aaroncareaga@gmail.com>
2015-12-15 20:58:53 -0700

    Need to fix unregistered user on dashboard

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -1,9 +1,11 @@
 class FeedController < ApplicationController
 
   def index
-    client = Instagram.client(:access_token => current_user[:oauth_token])
-    @user = current_user
-    tags = client.tag_search('skiing')
-    @recent_feed = client.tag_recent_media(tags[0].name)
+    if current_user
+      client = Instagram.client(:access_token => current_user[:oauth_token])
+      @user = current_user
+      tags = client.tag_search('berner')
+      @recent_feed = client.tag_recent_media(tags[0].name)
+    end
   end
 end
