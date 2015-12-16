2b2d988a3a271742eadf48034ed480f7548fb7a9
acareaga <aaroncareaga@gmail.com>
2015-12-16 09:43:34 -0700

    Fixed card show on feed

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -4,6 +4,7 @@ class FeedController < ApplicationController
     if current_user
       client = Instagram.client(:access_token => current_user[:oauth_token])
       @user = current_user
+      # binding.pry
       tags = client.tag_search('berner')
       @recent_feed = client.tag_recent_media(tags[0].name)
     end
