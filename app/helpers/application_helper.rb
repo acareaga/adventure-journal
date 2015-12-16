3f137d9f93d7e1348431119f210af3ba7f95f63b
acareaga <aaroncareaga@gmail.com>
2015-12-15 17:26:01 -0700

    Fixed login logout view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,9 +2,9 @@ module ApplicationHelper
 
   def login_logout_path
     if current_user
-      link_to "Logout", logout_path
+      link_to "logout", logout_path
     else
-      link_to "Login", login_path
+      link_to "login", login_path
     end
   end
 end
