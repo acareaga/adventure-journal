cc179a1e72568d40419d0c69037232215e9f0bf6
acareaga <aaroncareaga@gmail.com>
2015-12-15 18:01:14 -0700

    Gem not working for recent feed

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -1,5 +1,8 @@
 class FeedController < ApplicationController
 
   def index
+    client = Instagram.client(:access_token => current_user[:oauth_token])
+    @user = current_user
+    @recent_feed = client.media_popular({:count => 20})
   end
 end
